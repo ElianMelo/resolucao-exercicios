7823c84893d1e0917916491462dec146c6454e96
ElianMelo <elianmelomorais@gmail.com>
2018-12-05 14:34:39 -0200

    Altera o repositorio conforme o padrao

--- Personagem.java
+++ Personagem.java
@@ -1,3 +1,11 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 05/12/2018
+Objetivo sucinto do programa: Programa para demonstração do uso do THIS (Programação Orientada a Objetos)
+Referência ao enunciado/origem do exercício: Exercício prático com THIS
+https://docs.google.com/forms/d/e/1FAIpQLSckb5FSZXqMVsgf3gF831PDLcp0iZySindzJ1P4hkZq7wg0og/viewform
+Informações adicionais: https://goo.gl/FjGdN9
+*/
 public class Personagem {
     int vida = 2;
     int forca = 1;
