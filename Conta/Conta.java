1131fc44e39902629ee15dba42770c9e67b316a6
ElianMelo <elianmelomorais@gmail.com>
2018-12-06 16:41:40 -0200

    Altera o nome dos metodos get e set das classes Conta e TestaConta

--- Conta/Conta.java
+++ Conta/Conta.java
@@ -1,19 +1,19 @@
 /*
 Nome do autor: Elian Melo Morais
 Data de criação do arquivo: 05/12/2018
-Objetivo sucinto do programa: Cria a classe Conta que possui um atributo encapsulado pelos métodos define e pega
-Referência ao enunciado/origem do exercício: Paul Deitel, Harvey Deitel-Java - Como Programar-Pearson (2016) páginas: 91-94
+Objetivo sucinto do programa: Cria a classe Conta que possui um atributo encapsulado pelos métodos set e get
+Referência ao enunciado/origem do exercício: Paul Deitel, Harvey Deitel-Java - Como Programar-Pearson (2016) páginas: 56 a 63
 */
 public class Conta {
     private String nome; // variável da instância
 
     // Método para definir o nome do objeto
-    public void defineNome(String nome){
+    public void setNome(String nome){
         this.nome = nome; // Armazena o parâmetro nome no atributo nome
     }
 
     // Método para recuperar o nome do objeto
-    public String pegaNome(){
+    public String getNome(){
         return nome; // Retorna o nome para o chamador
     }
 }
--- Conta/TestaConta.java
+++ Conta/TestaConta.java
@@ -1,8 +1,8 @@
 /*
 Nome do autor: Elian Melo Morais
 Data de criação do arquivo: 05/12/2018
-Objetivo sucinto do programa: Manipula a classe conta, com os métodos pega e define, e Scanner
-Referência ao enunciado/origem do exercício: Paul Deitel, Harvey Deitel-Java - Como Programar-Pearson (2016) páginas: 91-94
+Objetivo sucinto do programa: Manipula a classe conta, com os métodos get e set, e Scanner
+Referência ao enunciado/origem do exercício: Paul Deitel, Harvey Deitel-Java - Como Programar-Pearson (2016) páginas: 56 a 63
 */
 import java.util.Scanner;
 
@@ -15,15 +15,15 @@ public class TestaConta {
         Conta minhaConta = new Conta();
 
         // Exibe o valor inicial do nome (null)
-        System.out.printf("Nome inicial e: %s%n%n", minhaConta.pegaNome());
+        System.out.printf("Nome inicial e: %s%n%n", minhaConta.getNome());
 
         // Solicita e lê o nome
         System.out.println("Por favor insira um nome: ");
         String nome = leitor.nextLine(); // Lê uma linha de texto
-        minhaConta.defineNome(nome); // Insere nome em minhaConta
+        minhaConta.setNome(nome); // Insere nome em minhaConta
         System.out.println(); // Gera saída de uma linha em branco
 
         // Exibe o nome armazenado no objeto minhaConta
-        System.out.printf("nome no objeto minhaConta e: %n%s%n", minhaConta.pegaNome());
+        System.out.printf("nome no objeto minhaConta e: %n%s%n", minhaConta.getNome());
     }
 }
