c71afdee27fa3cef6b39e11d51693708388958fb
ElianMelo <elianmelomorais@gmail.com>
2018-12-07 15:06:14 -0200

    Adiciona classes que simulam um registro de empregados

---
+++ Empregado/Empregado.java
@@ -0,0 +1,56 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 07/12/2018
+Objetivo sucinto do programa: Cria a classe Empregado que simula um registro de empregados
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+*/
+public class Empregado {
+    private int id;
+    private String primeiroNome;
+    private String segundoNome;
+    private int salario;
+
+    Empregado(int id, String primeiroNome, String segundoNome, int salario){
+        this.id = id;
+        this.primeiroNome = primeiroNome;
+        this.segundoNome = segundoNome;
+        this.salario = salario;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getPrimeiroNome(){
+        return primeiroNome;
+    }
+
+    public String getSegundoNome(){
+        return segundoNome;
+    }
+
+    public String getNomeCompleto(){
+        return (primeiroNome + " " + segundoNome);
+    }
+
+    public int getSalario(){
+        return salario;
+    }
+
+    public void setSalario(int salario){
+        this.salario = salario;
+    }
+
+    public int getSalarioAnual(){
+        return (salario * 12);
+    }
+
+    public int getAumentoSalario(int porcentagem){
+        int novoSalario = salario + salario * porcentagem / 100;
+        return novoSalario;
+    }
+
+    public String toString(){
+        return("Empregado[id=" + id + " primeiroNome=" + primeiroNome + " segundoNome=" + segundoNome + " salario=" + salario + "]");
+    }
+}
---
+++ Empregado/TestaEmpregado.java
@@ -0,0 +1,30 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 07/12/2018
+Objetivo sucinto do programa: Cria a classe TestaEmpregado que manipula objetos Empregado utilizando seus métodos
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+*/
+public class TestaEmpregado {
+
+    public static void main(String[] args){
+        Empregado empregado1 = new Empregado(1, "Elian", "Melo", 450);
+        Empregado empregado2 = new Empregado(2, "Ilton", "Batista", 1000);
+        Empregado empregado3 = new Empregado(3, "Fábio", "Amaro", 50000);
+
+        System.out.println("ID do empregado3: " + empregado3.getId());
+        System.out.println("primeiro nome do empregado1: " + empregado1.getPrimeiroNome());
+        System.out.println("segundo nome do empregado3: " + empregado3.getSegundoNome());
+        System.out.println("nome completo do empregado2: " + empregado2.getNomeCompleto());
+        System.out.println("salario do empregado1: " + empregado1.getSalario());
+        System.out.println("salario anual do empregado1: " + empregado1.getSalarioAnual());
+        System.out.println("salario do empregado1 aumentado em 10%: " + empregado1.getAumentoSalario(10));
+
+        empregado1.setSalario(1000);
+        System.out.println("salario do empregado1 atualizado: " + empregado1.getSalario());
+
+        System.out.println("informacoes do empregado1: " + empregado1.toString());
+        System.out.println("informacoes do empregado2: " + empregado2.toString());
+        System.out.println("informacoes do empregado3: " + empregado3.toString());
+
+    }
+}
