c298c2f9dfb8fe470dce8c43a32ac3a7edc21c3b
ElianMelo <elianmelomorais@gmail.com>
2018-12-13 17:10:24 -0200

    Adiciona classes que funcionam como uma biblioteca

---
+++ Livro/Livro.java
@@ -0,0 +1,215 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 11/12/2018
+Objetivo sucinto do programa: Cria a classe Livro que possui diversos metodos para manipulacao de Livros e array de Livros
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+*/
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private Date dataEmprestimo;
+    private Date dataDevolucao;
+    private String statusLivro;
+    private int quantidadeEmprestimos;
+
+    SimpleDateFormat conversor = new SimpleDateFormat("dd-MM-yyyy");
+
+    Livro(){}
+
+    Livro(int id, String titulo, String autor, Date dataEmprestimo, Date dataDevolucao, String statusLivro, int quantidadeEmprestimos){
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+        this.statusLivro = statusLivro;
+        this.quantidadeEmprestimos = quantidadeEmprestimos;
+    }
+
+    // Retorna o ID
+    public int getId(){
+        return id;
+    }
+
+    // Retorna o título
+    public String getTitulo(){
+        return titulo;
+    }
+
+    // Altera o título
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    // Retorna o autor
+    public String getAutor(){
+        return autor;
+    }
+
+    // Altera o autor
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+
+    // Retorna a data de emprestimo
+    public String getDataEmprestimo(){
+        String data = "Emprestimo realizado no dia: " + conversor.format(dataEmprestimo);
+        return data;
+    }
+
+    // Altera a data de emprestimo
+    public void setDataEmprestimo(Date dataEmprestimo){
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    // Retorna a data de devolucao
+    public String getDataDevolucao(){
+        String data = "Devolucao deve ser realizada no dia: " + conversor.format(dataDevolucao);
+        return data;
+    }
+
+    // altera a data de devolucao
+    public void setDataDevolucao(Date dataDevolucao){
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    // Retorna o status do livro
+    public String getStatusLivro(){
+        return statusLivro;
+    }
+
+    // Altera o status do livro
+    public void setStatusLivro(String statusLivro){
+        this.statusLivro = statusLivro;
+    }
+
+    // Retorna a quantidade de emprestimos
+    public int getQuantidadeEmprestimos(){
+        return quantidadeEmprestimos;
+    }
+
+    // Altera a quantidade de emprestimos
+    public void setQuantidadeEmprestimos(int quantidadeEmprestimos){
+        this.quantidadeEmprestimos = quantidadeEmprestimos;
+    }
+
+    // Realiza o emprestimo do livro
+    public String emprestarLivro(){
+        if(statusLivro == "disponivel"){
+            statusLivro = "emprestado";
+            Date dataAtual = new Date();
+            setDataEmprestimo(dataAtual);
+            long soma = dataEmprestimo.getTime() + (86400 * 7 * 1000);
+            Date dataAux = new Date(soma);
+            setDataDevolucao(dataAux);
+            quantidadeEmprestimos++;
+
+            return "O livro foi emprestado com sucesso\n" + "data de emprestimo: " + conversor.format(dataEmprestimo) + "\n" + "data de devolucao: " + conversor.format(dataDevolucao);
+        }else{
+            return "Falha ao emprestar o livro";
+        }
+    }
+
+    // Realiza a devolucao do livro
+    public String devolverLivro(){
+        if(statusLivro == "emprestado"){
+            statusLivro = "disponivel";
+            return "O livro foi devolvido com sucesso";
+        }else{
+            return "Falha ao devolver o livro";
+        }
+    }
+
+    // Realiza uma busca em um array de livros
+    public static String buscarLivro(Livro[] arr, String titulo, String autor){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].getTitulo() == titulo && arr[i].getAutor() == autor){
+                return "Livro encontrado na posicao: " + i;
+            }
+        }
+        return "Livro nao encontrado";
+    }
+
+    // Realiza uma exibicao dos livros emprestados de um array
+    public static String exibirLivrosEmprestados(Livro[] arr){
+        String livros = "Livros Emprestados:\n";
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].getStatusLivro() == "emprestado"){
+                livros += arr[i].getTitulo() + "\n";
+            }
+        }
+        return livros;
+    }
+
+    // Realiza a exibicao dos livos disponiveis de um array
+    public static String exibirLivrosDisponiveis(Livro[] arr){
+        String livros = "Livros Disponiveis:\n";
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].getStatusLivro() == "disponivel"){
+                livros += arr[i].getTitulo() + "\n";
+            }
+        }
+        return livros;
+    }
+
+    // Realiza uma exibicao dos livros atrasados de um array
+    public static String exibirLivrosAtrasados(Livro[] arr){
+        String livros = "Livros Atrasados:\n";
+        Date dataAtual = new Date();
+        for(int i = 0; i < arr.length; i++){
+            if(dataAtual.getTime() > arr[i].dataDevolucao.getTime() && arr[i].getStatusLivro() == "emprestado"){
+                livros += arr[i].getTitulo() + "\n";
+            }
+        }
+        return livros;
+    }
+
+    // Gera uma multa por dias atrasados baseado em uma taxa
+    public double gerarMulta(double taxaPorDiaAtrasado){
+        Date dataAtual = new Date();
+        if(dataAtual.getTime() > dataDevolucao.getTime()){
+            long diferenca = dataDevolucao.getTime() - dataAtual.getTime();
+            long dia = diferenca / (864 * 1 * 100000) * -1;
+            double preco = dia * taxaPorDiaAtrasado;
+            return preco;
+        }else{
+            return -1;
+        }
+    }
+
+    // Lista o titulo dos livros armazenados no array
+    public static String listarTodosLivros(Livro[] arr){
+        String livros = "Todos os Livros:\n";
+        for(int i = 0; i < arr.length; i++){
+            livros += arr[i].getTitulo() + "\n";
+        }
+        return livros;
+    }
+
+    // Exibe o livro mais emprestado no array
+    public static String exibeLivroMaisEmprestado(Livro[] arr){
+        String livro = "Livro mais emprestado: ";
+        int qtdEmp = 0;
+        int posicao = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(i == 0){
+                qtdEmp = arr[i].getQuantidadeEmprestimos();
+                posicao = i;
+            }
+            if(arr[i].getQuantidadeEmprestimos() > qtdEmp){
+                qtdEmp = arr[i].getQuantidadeEmprestimos();
+                posicao = i;
+            }
+        }
+        return livro += arr[posicao].getTitulo() + " com " + qtdEmp + " emprestimos";
+    }
+
+    // Cria o método toString
+    public String toString(){
+        return "Livro[id=" + id + " titulo=" + titulo + " autor=" + autor + " dataEmprestimo=" + conversor.format(dataEmprestimo) + " dataDevolucao=" + conversor.format(dataDevolucao) + " statusLivro=" + statusLivro + "]";
+    }
+}
---
+++ Livro/TestaLivro.java
@@ -0,0 +1,121 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 11/12/2018
+Objetivo sucinto do programa: Manipula 5 instancias livro dentre elas um array de livros, utilizando todos os metodos disponiveis
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+*/
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TestaLivro {
+
+    public static void main(String[] args) {
+
+        SimpleDateFormat conversor = new SimpleDateFormat("dd-MM-yyyy");
+
+        Date dataAtual = new Date();
+        Date dataInicio = new Date(dataAtual.getTime() - (864 * 10 * 100000));
+        Date dataFim = new Date(dataAtual.getTime() + (864 * 1 * 100000));
+        Date dataFimAtrasado1 = new Date(dataAtual.getTime() - (864 * 3 * 100000));
+        Date dataFimAtrasado2 = new Date(dataAtual.getTime() - (864 * 5 * 100000));
+
+        Livro[] harryPotter = new Livro[8];
+
+        harryPotter[0] = new Livro(0, "Harry potter e a pedra filosofal", "J. K. Rowling", dataInicio, dataFim, "emprestado", 10);
+        harryPotter[1] = new Livro(1, "Harry potter e a camara secreta", "J. K. Rowling", dataInicio, dataFimAtrasado1, "emprestado", 20);
+        harryPotter[2] = new Livro(2, "Harry potter e o prisioneiro de azkaban", "J. K. Rowling", dataInicio, dataFim, "disponivel", 2);
+        harryPotter[3] = new Livro(3, "Harry potter e o calice de fogo", "J. K. Rowling", dataInicio, dataFimAtrasado2, "disponivel", 7);
+        harryPotter[4] = new Livro(4, "Harry potter e a ordem da fenix", "J. K. Rowling", dataInicio, dataFim, "disponivel", 1);
+        harryPotter[5] = new Livro(5, "Harry potter e o enigma do principe", "J. K. Rowling", dataInicio, dataFimAtrasado2, "emprestado", 30);
+        harryPotter[6] = new Livro(6, "Colecao harry potter", "J. K. Rowling", dataInicio, dataFim, "disponivel", 50);
+        harryPotter[7] = new Livro(7, "Animais fantasticos e onde habitam", "J. K. Rowling", dataInicio, dataFim, "emprestado", 5);
+
+        Livro vivian = new Livro(8, "Vivian contra o apocalipse", "Katie Coyle", dataInicio, dataFimAtrasado2, "emprestado", 5);
+        Livro habito = new Livro(9, "O poder do habito", "Charles Duhigg", dataInicio, dataFim, "disponivel", 3);
+        Livro corvo = new Livro(9, "O corvo", "Edgar Allan Poe", dataInicio, dataFim, "disponivel", 7);
+        Livro rapido = new Livro(9, "Rapido e devagar", "Daniel Kahneman", dataInicio, dataFim, "emprestado", 2);
+
+        // To string inicial
+        System.out.println(vivian.toString());
+        System.out.println(habito.toString());
+        System.out.println(corvo.toString());
+        System.out.println(rapido.toString());
+        System.out.println(harryPotter[0].toString());
+        System.out.println(harryPotter[1].toString());
+        System.out.println(harryPotter[2].toString());
+        System.out.println("...");
+        System.out.println();
+
+        // Testes com vivian
+        System.out.println("--- Testes com vivian ---");
+        System.out.println();
+        System.out.println("Titulo do livro: " + vivian.getTitulo());
+        System.out.println("Status do livro: " + vivian.getStatusLivro());
+        System.out.println("Emprestar livro:\n" + vivian.emprestarLivro());
+        System.out.println("Devolver livro:\n" + vivian.devolverLivro());
+        System.out.println();
+
+        // Testes com get e set no livro corvo
+        System.out.println("--- Testes com corvo ---");
+        System.out.println();
+        System.out.println("ID: " + corvo.getId());
+        System.out.println("Título do livro: " + corvo.getTitulo());
+        corvo.setTitulo("O corvo edicao limitada");
+        System.out.println("Título do livro: " + corvo.getTitulo());
+        System.out.println("Autor do livro: " + corvo.getAutor());
+        corvo.setAutor("Poe, Edgar Allan");
+        System.out.println("Autor do livro: " + corvo.getAutor());
+        System.out.println("Data de emprestimo: " + corvo.getDataEmprestimo());
+        corvo.setDataEmprestimo(dataFimAtrasado2);
+        System.out.println("Data de emprestimo: " + corvo.getDataEmprestimo());
+        System.out.println("Data de devolucao: " + corvo.getDataDevolucao());
+        corvo.setDataDevolucao(dataFimAtrasado1);
+        System.out.println("Data de devolucao: " + corvo.getDataDevolucao());
+        System.out.println("Status do livro: " + corvo.getStatusLivro());
+        corvo.setStatusLivro("emprestado");
+        System.out.println("Status do livro: " + corvo.getStatusLivro());
+        System.out.println("Quantidade de emprestimos: " + corvo.getQuantidadeEmprestimos());
+        corvo.setQuantidadeEmprestimos(15);
+        System.out.println("Quantidade de emprestimos: " + corvo.getQuantidadeEmprestimos());
+        System.out.println();
+
+        // Testes com os metodos simples no livro rapido e habito
+        System.out.println("--- Testes com rapido e habito ---");
+        System.out.println();
+        System.out.println("Emprestar livro rapido:\n" + rapido.emprestarLivro());
+        System.out.println("Emprestar livro habito:\n" + habito.emprestarLivro());
+        System.out.println();
+
+        System.out.println("Devolver livro rapido:\n" + rapido.devolverLivro());
+        System.out.println("Devolver livro habito:\n" + habito.devolverLivro());
+        System.out.println();
+
+        // Testa os metodos de array com harryPotter
+        System.out.println("--- Testes com harry potter ---");
+        System.out.println();
+        System.out.println("Busca o livro Colecao harry potter da J. K. Rowling\n" + Livro.buscarLivro(harryPotter, "Colecao harry potter", "J. K. Rowling"));
+        System.out.println("Busca o livro animais fantasticos 2 da J. K. Rowling\n" + Livro.buscarLivro(harryPotter, "animais fantasticos 2", "J. K. Rowling"));
+        System.out.println();
+
+        System.out.println(Livro.exibirLivrosEmprestados(harryPotter));
+        System.out.println(Livro.exibirLivrosDisponiveis(harryPotter));
+        System.out.println(Livro.exibirLivrosAtrasados(harryPotter));
+        System.out.println(Livro.exibeLivroMaisEmprestado(harryPotter));
+        System.out.println();
+
+        System.out.println("Cobra R$2.00 para cada dia atrasado do livro: " + harryPotter[1].getTitulo() + "\nValor total: R$" + harryPotter[1].gerarMulta(2));
+        System.out.println("Cobra R$5.00 para cada dia atrasado do livro: " + harryPotter[5].getTitulo() + "\nValor total: R$" + harryPotter[5].gerarMulta(5));
+        System.out.println();
+
+        System.out.println(Livro.listarTodosLivros(harryPotter));
+
+        System.out.println(vivian.toString());
+        System.out.println(habito.toString());
+        System.out.println(corvo.toString());
+        System.out.println(rapido.toString());
+        System.out.println(harryPotter[0].toString());
+        System.out.println(harryPotter[1].toString());
+        System.out.println(harryPotter[2].toString());
+        System.out.println("...");
+    }
+}
