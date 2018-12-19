9c50f5d30d6ccefec9bdd5cd7128b25e4b4942bf
ElianMelo <elianmelomorais@gmail.com>
2018-12-19 14:51:20 -0200

    Adiciona o tratamento da excecao IllegalArgument Exception

---
+++ Tratamento de excecoes/Demonstra_IllegalArgumentException.java
@@ -0,0 +1,30 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 18/12/2018
+Objetivo sucinto do programa: Demonstrar o tratamento da exceção IllegalArgument Exception
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1cCI5x1i5kef9dWZKrvrobbZddDXuLhhK5Kw8Pdj-ouA/edit
+*/
+import java.lang.IllegalArgumentException;
+
+class Demonstra_IllegalArgumentException {
+    private final int valor;
+
+    public Demonstra_IllegalArgumentException(int valor) {
+        if (valor != 1) {
+            throw new IllegalArgumentException(Integer.toString(valor));
+        }
+            this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public static void main(String[] args){
+        try {
+            Demonstra_IllegalArgumentException percentage = new Demonstra_IllegalArgumentException(2);
+        } catch(IllegalArgumentException e){
+            System.out.println("Argumento inválido");
+        }
+    }
+}
