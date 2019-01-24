1b1b3bc7ed168070953bddce0d2ccd411562e6ca
ElianMelo <elianmelomorais@gmail.com>
2019-01-24 14:30:56 -0200

    Adiciona as classes Livro e Autor e AutorLivroTeste usando composicao

---
+++ autor-e-livro/AutorLivroTeste.java
@@ -0,0 +1,32 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 23/01/2019
+Objetivo sucinto do programa: Criar um classe que testa as classes Livro e Autor
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1wSPKmVGsvzHlM13SY_IxCcqA1AhFboDvSy6M6NqOTfY/edit
+*/
+public class AutorLivroTeste {
+	
+	public static void main(String[] args) {
+		Autor autor1 = new Autor("John Carreyrou", "john-carreyrou@gmail.com", 'm');
+		Autor autor2 = new Autor("Tara Westover", "tara-westover@gmail.com", 'f');
+		
+		Livro livro1 = new Livro("Bad Blood", autor1, 59.90, 50);
+		Livro livro2 = new Livro("A Menina da Montanha", autor2, 38.61, 100);
+		
+		System.out.println(autor1.toString());
+		System.out.println(autor2.toString());
+		System.out.println(livro1.toString());
+		System.out.println(livro2.toString());
+		
+		livro1.setPreco(100);
+		System.out.println("Preço do livro1: " + livro1.getPreco());
+		livro2.setQuantidade(10);
+		System.out.println("Quantidade do livro2: " + livro2.getQuantidade());
+		System.out.println("Autor do livro2: " + livro2.getAutor());
+		
+		autor1.setEmail("joao-carr@gmail.com");
+		System.out.println(autor1.getEmail());
+		System.out.println(autor2.getGenero());
+	}
+
+}
---
+++ autor-e-livro/Livro.java
@@ -0,0 +1,49 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 23/01/2019
+Objetivo sucinto do programa: Criar um classe que simula um livro com seu respectivo autor
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1wSPKmVGsvzHlM13SY_IxCcqA1AhFboDvSy6M6NqOTfY/edit
+*/
+public class Livro {
+	private String nome;
+	private Autor autor;
+	private double preco;
+	private int quantidade = 0;
+	
+	Livro(String nome, Autor autor, double preco) {
+		this.nome = nome;
+		this.autor = autor;
+		this.preco = preco;
+	}
+	
+	Livro(String nome, Autor autor, double preco, int quantidade) {
+		this.nome = nome;
+		this.autor = autor;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public Autor getAutor() {
+		return autor;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	@Override
+	public String toString() {
+		return "Livro [nome=" + nome + "," + autor.toString() + ", preco=" + preco + ", quantidade=" + quantidade + "]";
+	}
+}
